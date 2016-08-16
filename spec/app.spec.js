fa0c7469d4053107896a2e5f12867f48696f1ece
Zhezhang Fu <zephitez@gmail.com>
2016-08-16 09:32:16 +0800

    change to personal api

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -14,24 +14,9 @@ describe("Express Server API", function(){
     it("returns a correct json format", function(done){
       supertest(app)
         .get('/members')
-        .expect([ { name: 'Jun Kai',
-                    email: 'junkaiii@gmail.com',
-                    favcolor: 'green' },
-                    { name: 'Veronica Ler',
-                    email: 'veronler@hotmail.com',
-                    favcolor: 'white' },
-                    { name: 'Fu Zhezhang',
+        .expect([ { name: 'Fu Zhezhang',
                     email: 'zephitez@gmail.com',
-                    favcolor: 'blue' },
-                    { name: 'Tan Kai Lin',
-                    email: 'kailin.tan.2010@gmail.com',
-                    favcolor: 'blue' },
-                    { name: 'Nicholas Lai',
-                    email: 'nicholas.lai1985@gmail.com',
-                    favcolor: 'blue' },
-                    { name: 'Barnabas Chan',
-                    email: 'barny@post.com',
-                    favcolor: 'grey' } ],done);
-                    });
+                    favcolor: 'blue' } ]);
                   });
+});
 });

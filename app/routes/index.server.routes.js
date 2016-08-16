853749b49619f7f07503e9715cf5eb4cb38603d6
Zhezhang Fu <zephitez@gmail.com>
2016-08-16 09:59:38 +0800

    fix cross origin issues

--- app/routes/index.server.routes.js
+++ app/routes/index.server.routes.js
@@ -1,5 +1,11 @@
 module.exports = function(app) {
   var membersController = require('../controllers/members.server.controller');
 
+  app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
   app.get('/members', membersController.index);
 };
